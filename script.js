d9c1fa841251ec546c3b00a1f53a0a7d60b8651a
Adam Byrne <adam.r.byrne@gmail.com>
2015-12-30 17:26:49 +0000

    Added files
    added html,js,css files

---
+++ script.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+  var Timer = document.getElementById('time');
+  var Start = document.getElementById('start');
+  var Restart = document.getElementById('restart');
+
+  var time = 0;
+  var running = 0;
+
+  Start.onclick = function() {
+    if(Start.innerHTML === "Play" && running === 0) {
+      running = 1;
+      increment()
+      Start.innerHTML = "Pause";
+    }else{
+      running = 0
+      Start.innerHTML = "Play";
+    }
+  };
+
+  Restart.onclick = function() {
+    Start.innerHTML = "Play";
+    running = 0;
+    time = 0;
+    mins = 0;
+    secs = 0;
+    tenths = 0;
+    Timer.innerHTML = mins+":"+secs+":"+tenths;
+  };
+
+  function increment() {
+    if(running === 1) {
+      setTimeout(function() {
+        time++;
+        mins = Math.floor(time / 10 / 60);
+        secs = Math.floor(time / 10);
+        tenths = time % 10;
+        Timer.innerHTML = mins+":"+secs+":"+tenths;
+        increment();
+      },100);
+    }
+  }
+}
